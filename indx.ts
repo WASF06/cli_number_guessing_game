6a79c07b33bd5a8045b6376779b1674ef3111d1e
waja.sial@gmail.com <waja.sial@gmail.com>
2024-04-04 01:11:04 +0500

    created cli gussing game

---
+++ indx.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random()*6 + 1);
+
+const answers = await inquirer.prompt([
+    { 
+        name:"userguessednumber",
+        type:"number",
+        message:"please guess a number between 1-6:",
+    }
+    ]);
+    
+    if (answers.userguessednumber === randomNumber){
+         console.log ("congratulation! you guessed right number.");
+    }
+    else{
+        console.log("you guessed wrong number");
+    }
